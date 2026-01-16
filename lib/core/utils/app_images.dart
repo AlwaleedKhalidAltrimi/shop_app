099e2ca0b988ad6203548fd7b37b17c9ba2ad915
AlwaleedKhalidAltrimi <alwaleedaltrimi@gmail.com>
2026-01-16 14:49:35 +0300

    Setup App

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  static const String imagesCredit = "assets/images/credit.png";
+
+  static const String imagesEmail = "assets/images/email.png";
+
+  static const String imagesFacebook = "assets/images/facebook.png";
+
+  static const String imagesForgetPassword = "assets/images/forget_password.png";
+
+  static const String imagesGoogle = "assets/images/google.png";
+
+  static const String imagesHeart = "assets/images/heart.png";
+
+  static const String imagesLock = "assets/images/lock.png";
+
+  static const String imagesPaypal = "assets/images/paypal.png";
+
+  static const String imagesRate = "assets/images/rate.png";
+
+  static const String imagesSearchEmptyDark = "assets/images/search_empty_dark.png";
+
+  static const String imagesSearchEmptyLight = "assets/images/search_empty_light.png";
+
+  static const String imagesShop = "assets/images/shop.png";
+
+  static const String imagesUser = "assets/images/user.png";
+}
+
