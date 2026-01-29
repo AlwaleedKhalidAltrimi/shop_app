342bfdfd200717a12f79ffe133c31c7f22465d5b
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2026-01-29 14:49:33 +0300

    Add API base URL to constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,4 +4,6 @@ class AppConstants {
   static const String validationName = r'^[a-z A-Z]+$';
   static const String validationEmail =
       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+
+  static const String baseUrl = 'https://fakestoreapi.com';
 }
