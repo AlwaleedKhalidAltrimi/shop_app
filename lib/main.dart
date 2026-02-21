500d917dc1c9cd58b87af05da3e5fe72a245c620
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2026-02-21 14:36:48 +0300

    Modify main.dart to initialize GetStorageUtil instead of GetStorage directly

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get_storage/get_storage.dart';
 import 'app/shop_app.dart';
 import 'core/functions/check_auth_state.dart';
 import 'core/functions/check_state_changes.dart';
 
+import 'core/functions/get_storage.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,7 +18,7 @@ void main() async {
     debugPrint("Could not load .env file: \$e");
   }
 
-  await GetStorage.init();
+  await GetStorageUtil.init();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   checkStateChanges();
 
