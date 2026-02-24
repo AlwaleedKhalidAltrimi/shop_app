04ed9be852df915043cf721f0deeff117306f94b
Alwaleed Kalid <157881255+AlwaleedKhalidAltrimi@users.noreply.github.com>
2026-02-24 10:43:55 +0300

    enhance product controller with favorites and search features

--- lib/features/home/presentation/controllers/product_controller.dart
+++ lib/features/home/presentation/controllers/product_controller.dart
@@ -1,54 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import '../../../../core/constants/app_constants.dart';
 import '../../data/models/product_models.dart';
 import '../../data/services/product_services.dart';
+import '../../../../core/functions/get_storage.dart';
 
 class ProductController extends GetxController {
-  var productList = <ProductModel>[].obs;
-  var isLoading = true.obs;
+  final RxList<ProductModel> productList = <ProductModel>[].obs;
+  final RxList<ProductModel> favouritesList = <ProductModel>[].obs;
+  final RxList<ProductModel> searchList = <ProductModel>[].obs;
 
-  //search
-  var searchList = <ProductModel>[].obs;
-  TextEditingController searchTextController = TextEditingController();
+  final RxBool isLoading = false.obs;
+  final RxString errorMessage = ''.obs;
 
+  final RxString searchText = ''.obs;
+  final TextEditingController searchTextController = TextEditingController();
+
+  // var storage = GetStorageUtil();
   @override
   void onInit() {
     super.onInit();
-    getAllProductss();
+
+    List? storedShoppings = GetStorageUtil.read<List>(
+      AppConstants.isFavouritesList,
+    );
+
+    if (storedShoppings != null) {
+      favouritesList.value = storedShoppings
+          .map((e) => ProductModel.fromJson(e))
+          .toList();
+    }
+    fetchAllProducts();
   }
 
-  void getAllProductss() async {
-    var products = await AllProductsService.getAllProducts();
-
+  void fetchAllProducts() async {
     try {
-      isLoading(true);
-      if (products.isNotEmpty) {
-        productList.addAll(products);
-      }
+      isLoading.value = true;
+      errorMessage.value = '';
+      final products = await AllProductsService.getAllProducts();
+      productList.addAll(products);
+    } catch (e) {
+      errorMessage.value = "Unable to load products. Please try again.";
+      debugPrint("Fetch products error: $e");
     } finally {
-      isLoading(false);
+      isLoading.value = false;
     }
   }
 
   //Search Bar Logic
-
-  void addSearchToList(String searchName) {
-    searchName = searchName.toLowerCase();
-
-    searchList.value = productList.where((search) {
-      var searchTitle = search.title.toLowerCase();
-      var searchPrice = search.price.toString().toLowerCase();
-
-      return searchTitle.contains(searchName) ||
-          searchPrice.toString().contains(searchName);
-    }).toList();
-
-    update();
+  void searchProduct(String value) {
+    searchText.value = value;
+    final text = value.trim().toLowerCase();
+    searchList.value = productList
+        .where(
+          (product) =>
+              product.title.toLowerCase().contains(text) ||
+              product.price.toString().contains(text),
+        )
+        .toList();
   }
 
-  void clearSearch() {
+  void clearSearchField() {
     searchTextController.clear();
-    addSearchToList("");
+    searchProduct("");
+  }
+
+  // Logic for Favourites Screen
+  void toggleFavourites(int productId) async {
+    var existingIndex = favouritesList.indexWhere(
+      (element) => element.id == productId,
+    );
+
+    if (existingIndex >= 0) {
+      favouritesList.removeAt(existingIndex);
+      await GetStorageUtil.remove(AppConstants.isFavouritesList);
+    } else {
+      favouritesList.add(
+        productList.firstWhere((element) => element.id == productId),
+      );
+
+      // await GetStorageUtil.write(AppConstants.isFavouritesList, favouritesList);
+    }
+    await GetStorageUtil.write(
+      AppConstants.isFavouritesList,
+      favouritesList.map((e) => e.toJson()).toList(),
+    );
+  }
+
+  bool isFavourites(int productId) {
+    return favouritesList.any((element) => element.id == productId);
+  }
+
+  @override
+  void onClose() {
+    searchTextController.dispose();
+    super.onClose();
   }
 }
